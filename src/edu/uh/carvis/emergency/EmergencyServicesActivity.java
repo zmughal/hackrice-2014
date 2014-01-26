6bb998bc713cc9622bafbd322bb05c5bacf81ee7
Eric Siebeneich <eric.siebeneich@gmail.com>
2014-01-25 21:16:39 -0600

    fixed manifest

---
+++ src/edu/uh/carvis/emergency/EmergencyServicesActivity.java
@@ -0,0 +1,14 @@
+package edu.uh.carvis.emergency;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: Eric
+ * Date: 1/25/14
+ */
+public class EmergencyServicesActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
