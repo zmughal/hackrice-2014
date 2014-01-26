e29cb9aa506c6c1710d1bc2f0c86f7c5c77379e4
Eric Siebeneich <eric.siebeneich@gmail.com>
2014-01-25 23:18:13 -0600

    Emergency Services module

--- src/edu/uh/carvis/emergency/EmergencyServicesActivity.java
+++ src/edu/uh/carvis/emergency/EmergencyServicesActivity.java
@@ -1,14 +1,60 @@
 package edu.uh.carvis.emergency;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import edu.uh.carvis.R;
 
 /**
  * User: Eric
  * Date: 1/25/14
  */
 public class EmergencyServicesActivity extends Activity {
+
+	private SeekBar seekBar;
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.emergency_services);
+
+		seekBar = (SeekBar)findViewById(R.id.dial911);
+		seekBar.setOnSeekBarChangeListener(new Dial911Listener());
+	}
+
+	class Dial911Listener implements SeekBar.OnSeekBarChangeListener {
+
+		private static final double percentage = 0.85;
+
+		private double getPercentage(SeekBar seekBar, int progress) {
+			return ((double)progress) / seekBar.getMax();
+		}
+
+		private void dial911() {
+			Uri uri = Uri.parse("tel://2817815206");
+			Intent intent = new Intent(Intent.ACTION_CALL, uri);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+			startActivity(intent);
+		}
+
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+			if (getPercentage(seekBar, progress) > percentage) {
+				dial911();
+			}
+		}
+
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+			seekBar.setProgress(0);
+		}
 	}
 }
